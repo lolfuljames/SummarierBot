a9f82e26f75b0ea1ebe63131949a4f5e2018603c
James Tan <jamestancheemin98@gmail.com>
2017-09-24 18:12:39 +0800

    added folder directory

--- main1.py
+++ main1.py
@@ -6,15 +6,17 @@ import os
 
 
 bot = telepot.Bot("423773801:AAEMhmJhn8jdZt9GtJ7xkX-49wJJ9rqeNJo")
+path = os.getcwd()
 
 
 def startSummarise(chat_id):
-	if not os.path.exists(chat_id + ".txt"):
-		newfile=open(chat_id + ".txt","w+")
+	if not os.path.exists(path +"/messages/"+ chat_id + ".txt"):
+		newfile=open(path+ chat_id + ".txt")
 		newfile.close()
 	else :
-		doc=open(chat_id + ".txt","r")
+		doc=open(path +"/messages/"+ chat_id + ".txt","r")
 		document=doc.read()
+		doc.close()
 		print (document)
 		summaryString=textranker(document)
 		if summaryString == "[]": #output will be [] if there is not enough data
@@ -27,13 +29,14 @@ def handle(msg):
 	if content_type == "text":
 		chat_id = str(chat_id)
 		messageString = msg["text"]
+		print(path)
 		if messageString == "/summarise":
 			startSummarise(chat_id)
 		elif messageString == "/clear":
-			os.remove(chat_id + ".txt")
+			os.remove(path +"/messages/" + chat_id + ".txt")
 		elif messageString[0] != "/": #ignore messages starting with / 
 			print("received " + messageString + " from " + chat_id )
-			store=open(chat_id + ".txt","a+") 
+			store=open(path  +"/messages/"+ chat_id + ".txt","a+") 
 			store.write(msg["text"] + " ")
 			store.close()
 MessageLoop(bot,{"chat":handle}).run_as_thread()
